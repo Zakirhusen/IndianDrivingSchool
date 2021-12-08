592425f236a3d6749f2182f0a43df0aee1a0e869
Zakirhusen <72880935+Zakirhusen@users.noreply.github.com>
2021-12-08 13:00:04 +0530

    deploy testing "dummy"

--- src/Section4.js
+++ src/Section4.js
@@ -1,52 +1,58 @@
 import React from "react";
 import icon1 from "./images/icon-prize.png";
 import icon2 from "./images/icon-time.png";
+import icon3 from "./images/icon-students.png" ;
+import icon4 from "./images/icon-goal.png";
+import icon5 from "./images/icon-gear.png";
+import icon6 from "./images/icon-brake.png";
+import icon7 from "./images/icon-documents.png";
+import icon8 from "./images/icon-cash.png";
 
 const dataArray = [
   {
     heading: "BEST SERVICE",
     description: "Provides the best service to be expert in driving",
-    imgSrc: {icon1},
+    imgSrc: icon1,
   },
   {
     heading: "BEST TIME TRAINING",
     description:
       "You can choose your team on weekdays after work or on weekends",
-    imgSrc: {icon2},
+    imgSrc: icon2
   },
   {
     heading: "WE TRAIN ALL AGES",
     description: "we take into account the age of individual moments.",
-    imgSrc: "./images/icon-students.png",
+    imgSrc: icon3,
   },
   {
     heading: "INDIVIDUAL APPROACH",
     description: "To everyone we have our own approach.",
-    imgSrc: "./images/icon-goal.png",
+    imgSrc: icon4,
   },
   {
     heading: "Experienced INSTRUCTOR",
     description:
       "All our Instructors are DMV Licensed, mentally and physically fit and have decade or more of Instruction History.",
-    imgSrc: "./images/icon-gear.png",
+    imgSrc: icon5,
   },
   {
     heading: "DUPLICATE PEDALS",
     description:
       "Cars equipped with duplicate pedals - our instructor always insures you.",
-    imgSrc: "./images/icon-brake.png",
+    imgSrc: icon6,
   },
   {
     heading: "ALL DOCUMENTS",
     description:
       "The full package of documents at the end - you get all the documents at the end of",
-    imgSrc: "./images/icon-documents.png",
+    imgSrc: icon7,
   },
   {
     heading: "Easy payment",
     description:
       "We allow you to decide when and how you want to pay. We accept all major forms of payments.",
-    imgSrc: "./images/icon-cash.png",
+    imgSrc: icon8,
   },
 ];
 const Section4 = () => {
